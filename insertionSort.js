608ad0a1ff0515d57fdf0432f8a43a1cab82c5f5
ButuzovDima <butuzovdmitry31@gmail.com>
2017-10-02 23:49:39 +0300

    insertionSort

---
+++ insertionSort.js
@@ -0,0 +1,15 @@
+let insertionSort = function(array){
+    let p;
+    for(let i = 1; i < array.length; i++){
+        for(let j = i; j >= 0; j--){
+            if(array[j - 1] > array[j]){
+                p = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = p;
+            }
+        }
+    }
+    return array;
+};
+
+console.log(insertionSort([2,0,-1,5,7,3,4]));
