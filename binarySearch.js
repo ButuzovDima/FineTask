cef8b69670b9ce60272402a8e352eb4df4849763
ButuzovDima <butuzovdmitry31@gmail.com>
2017-10-03 23:04:32 +0300

    binarySearch

---
+++ binarySearch.js
@@ -0,0 +1,10 @@
+let binarySearch = function(array, f){
+    let l = 0, r = array.length - 1;
+    let k;
+    while(l <= r){
+        k = Math.floor((r+l)/2);
+        if(f === array[k]) return k;
+        if(f > array[k]) l = k + 1;
+        if(f < array[k]) r = k - 1;
+    }
+};
